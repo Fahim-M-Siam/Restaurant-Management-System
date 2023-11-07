61325857425137a2664ed2675db9428a47399be5
Fahime <fahim.mohammad.siam@gmail.com>
2023-11-08 01:03:37 +0600

    Fixed the navbar

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -43,53 +43,126 @@ const Navbar = () => {
         <Link to="/">TasteHarmony Cafe</Link>
       </div>
 
-      <div className="flex-none hidden lg:block">
-        <div className="flex items-center gap-2">
-          {/* Navbar menu content here */}
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive
-                ? "btn btn-outline bg-[#C59D5F] text-white btn-sm"
-                : "btn btn-ghost btn-sm"
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/allfooditems"
-            className={({ isActive }) =>
-              isActive
-                ? "btn btn-outline bg-[#C59D5F] text-white btn-sm"
-                : "btn btn-ghost btn-sm"
-            }
-          >
-            Foods
-          </NavLink>
-          <NavLink
-            to="/blog"
-            className={({ isActive }) =>
-              isActive
-                ? "btn btn-outline bg-[#C59D5F] text-white btn-sm"
-                : "btn btn-ghost btn-sm"
-            }
-          >
-            Blogs
-          </NavLink>
-
-          {currentUser?.email ? (
-            <div className="dropdown dropdown-end ">
-              <label tabIndex={0} className="cursor-pointer">
-                <div className="avatar">
-                  <div className="w-10 rounded-full">
-                    <img src={currentUser.photoURL} />
-                  </div>
+      <div className="hidden lg:flex items-center gap-2">
+        {" "}
+        {/* This will be displayed on larger screens */}
+        {/* Navbar menu content here */}
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "btn btn-outline bg-[#C59D5F] text-white btn-sm"
+              : "btn btn-ghost btn-sm"
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/allfooditems"
+          className={({ isActive }) =>
+            isActive
+              ? "btn btn-outline bg-[#C59D5F] text-white btn-sm"
+              : "btn btn-ghost btn-sm"
+          }
+        >
+          Foods
+        </NavLink>
+        <NavLink
+          to="/blog"
+          className={({ isActive }) =>
+            isActive
+              ? "btn btn-outline bg-[#C59D5F] text-white btn-sm"
+              : "btn btn-ghost btn-sm"
+          }
+        >
+          Blogs
+        </NavLink>
+        {currentUser?.email ? (
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="cursor-pointer">
+              <div className="avatar">
+                <div className="w-10 rounded-full">
+                  <img src={currentUser.photoURL} alt="User Avatar" />
                 </div>
-              </label>
-              <div
-                tabIndex={0}
-                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+              </div>
+            </label>
+            <div
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <NavLink
+                to="/user/addedfooditems"
+                className="px-4 py-2 hover:bg-base-300 rounded-lg"
               >
+                My Foods
+              </NavLink>
+              <NavLink
+                to="/user/addfood"
+                className="px-4 py-2 hover-bg-base-300 rounded-lg"
+              >
+                Add Food
+              </NavLink>
+              <NavLink
+                to="/user/orders"
+                className="px-4 py-2 hover:bg-base-300 rounded-lg"
+              >
+                Orders
+              </NavLink>
+
+              <div
+                onClick={handleLogOut}
+                className="cursor-pointer text-red-500 px-4 py-2 hover:bg-base-300 rounded-lg"
+              >
+                Logout
+              </div>
+            </div>
+          </div>
+        ) : (
+          <>
+            <NavLink
+              to="/login"
+              className={({ isActive }) =>
+                isActive ? "btn bg-[#C59D5F] btn-sm" : "btn btn-ghost btn-sm"
+              }
+            >
+              Login
+            </NavLink>
+            <div className="avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  src="https://i.ibb.co/bLrMRXp/user.png"
+                  alt="User Avatar"
+                />
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+
+      <div className="lg:hidden">
+        {" "}
+        {/* This will be displayed on smaller screens */}
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="cursor-pointer">
+            <div className="avatar">
+              <div className="w-10 rounded-full">
+                {currentUser?.email ? (
+                  <img src={currentUser.photoURL} alt="User Avatar" />
+                ) : (
+                  <img
+                    src="https://i.ibb.co/bLrMRXp/user.png"
+                    alt="User Avatar"
+                  />
+                )}
+              </div>
+            </div>
+          </label>
+          <div
+            tabIndex={0}
+            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {currentUser?.email ? (
+              <>
                 <NavLink
                   to="/user/addedfooditems"
                   className="px-4 py-2 hover:bg-base-300 rounded-lg"
@@ -108,35 +181,22 @@ const Navbar = () => {
                 >
                   Orders
                 </NavLink>
-
                 <div
                   onClick={handleLogOut}
                   className="cursor-pointer text-red-500 px-4 py-2 hover:bg-base-300 rounded-lg"
                 >
                   Logout
                 </div>
-              </div>
-            </div>
-          ) : (
-            <>
+              </>
+            ) : (
               <NavLink
                 to="/login"
-                className={({ isActive }) =>
-                  isActive ? "btn bg-[#C59D5F] btn-sm" : "btn btn-ghost btn-sm"
-                }
+                className="px-4 py-2 hover:bg-base-300 rounded-lg"
               >
                 Login
               </NavLink>
-              <div className="avatar">
-                <div className="w-10 rounded-full">
-                  <img
-                    src="https://i.ibb.co/bLrMRXp/user.png"
-                    alt="User Avatar"
-                  />
-                </div>
-              </div>
-            </>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </div>
